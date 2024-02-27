73c86ecf9ef076e9cffea4d1f4e80d7410809a32
Olga <samoylovaolga1111@icloud.com>
2024-02-27 20:21:41 +0300

    Added RTL FIFO

--- fifo.sv
+++ fifo.sv
@@ -28,12 +28,58 @@ NUM_CELLS = 2**(ADDR_WIDTH)
 input logic clk,
 input logic rst_n,
 input logic [T_DATA_WIDTH - 1:0] data_in,
-input logic wr_en,
-input logic rd_en,
+input logic s_valid,
+input logic m_ready,
+
 output logic [T_DATA_WIDTH - 1:0] data_out,
-output logic empty,
-output logic [NUM_CELLS:0] full
+output logic m_valid,
+output logic s_ready
     );
+
+logic [T_DATA_WIDTH - 1:0] data_fifo [NUM_CELLS - 1:0];
+logic [ADDR_WIDTH - 1:0] wr_pointer;
+logic [ADDR_WIDTH - 1:0] rd_pointer;
+
+//Initialization of signal wr_pointer
+always_ff @(posedge clk)
+if (~rst_n)
+    begin
+        wr_pointer <= 0;
+    end
+else if (s_valid && s_ready)
+    begin
+        wr_pointer <= wr_pointer + 1;
+        data_fifo [wr_pointer] <= data_in;
+    end
+
+//Initialization of signal rd_pointer
+               
+always_ff @(posedge clk)
+if (~rst_n)
+    begin
+        rd_pointer <= 0;
+    end
+else if (m_valid && m_ready)
+    begin
+        rd_pointer <= rd_pointer + 1;
+        data_out <= data_fifo [rd_pointer];
+    end
+    
+//initialization of signals s_ready and m_valid
+logic [NUM_CELLS - 1:0] cnt;
+
+always_ff @(posedge clk)
+    begin
+if (~rst_n)
+    cnt <= '0; 
+else if (s_valid && ~m_ready && cnt != NUM_CELLS - 1)
+    cnt <= cnt + 1'd1;
+else if (~s_valid && m_ready && cnt != 0)
+    cnt <= cnt - 1'd1;
+end
+
+assign s_ready = ~(cnt == NUM_CELLS - 1);
+assign m_valid = ~(cnt == 0);
 
 
 endmodule
